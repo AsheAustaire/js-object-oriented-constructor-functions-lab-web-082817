244ecb247cc2ca3b2b27536a46adcc24c2663de5
Ashe Austaire <AsheAustaire@gmail.com>
2017-10-11 08:35:33 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+function Scooter(year, color, model) {
+  this.year = 2015
+  this.color = 'red'
+  this.model = 'swoosh'
+}
+
+function Driver(name, age, experience) {
+  this.name = 'Allison'
+  this.age = '16 years'
+  this.experience = '2 years'
+}
+
+function PickupLocation(poshHotel, address, city) {
+  this.address = '123 Broadway'
+  this.city = 'New York City'
+
+}
